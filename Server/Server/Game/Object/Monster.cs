52b21a52204abe43022652c6448168c5c2490fc2
JuyeongKim <juyeong1996@gravity.co.kr>
2022-04-06 19:00:37 +0900

    몬스터 작업중

--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -4,14 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using UnityEngine;
 
 namespace Server.Game
 {
     public class Monster : BaseActor
     {
+        protected Vector3 _spawnPosition;
+        protected Vector3 _spawnDirection;
+
+        protected GameObject _target;
+
         public Monster()
         {
             ObjectType = GameObjectType.Monster;
+        }
+
+        public override void SyncPos()
+        {
+            base.SyncPos();
+
+            _spawnPosition = _position;
+            _spawnDirection = _direction;
         }
 
         public override void OnDamaged(GameObject attacker, int damage)
